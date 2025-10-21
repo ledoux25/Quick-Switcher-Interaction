0633271d55e7f9761780ad373bbac6daabd8cadd
ledoux25 <sanguojoseph17@gmail.com>
2025-10-21 18:07:54 -0400

    Initial Commit

---
+++ QuickSwitcherInteraction/QuickSwitcherInteractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSwitcherInteractionApp.swift
+//  QuickSwitcherInteraction
+//
+//  Created by Sanguo Joseph Ledoux on 10/21/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickSwitcherInteractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickSwitcherInteractionTests/QuickSwitcherInteractionTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSwitcherInteractionTests.swift
+//  QuickSwitcherInteractionTests
+//
+//  Created by Sanguo Joseph Ledoux on 10/21/25.
+//
+
+import Testing
+@testable import QuickSwitcherInteraction
+
+struct QuickSwitcherInteractionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
